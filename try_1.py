b46210292ff9c31859defa5a66681e09b1700040
kale.kaji <bsnbipendra@gmail.com>
2021-07-08 16:14:24 -0400

    Added all the functions together and got the ouput. Requires to look at times where pump is turned off and speed constant

--- try_1.py
+++ try_1.py
@@ -1,5 +1,6 @@
 import numpy as np
-import warnings 
+import warnings
+from numpy.lib.utils import info 
 warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 
 
@@ -10,7 +11,7 @@ from scipy.spatial import distance
 from scipy.spatial import distance
 import re
 import math
-x="G01 Z 0.431 F$global[7]" 
+
 
 constant_print={"layer_height":0.3, 
 "f_layer_height":0.4, 
@@ -23,25 +24,63 @@ constant_print={"layer_height":0.3,
 dict_axes={"X":[],"Y":[],"Z":[]}
 
 
-
+list_extrusion=[]
 pump_status=0
-info_array=np.array([1,0,[(0,0,0)]],dtype=object,ndmin=2)
+info_tuple=([1,0,[(0,0,0)],0])
+default_dict={"X":0,"Y":0,"Z":0}
 #print("At the start:",info_array)
 
 
 
 def calculation(info_coord):
+    global list_extrusion,info_tuple
     area_road=(((constant_print["def_layer_width"]-constant_print["layer_height"])*constant_print["layer_height"])+(math.pi*math.pow(constant_print["layer_height"]/2,2)))
     fil_area=math.pi*math.pow(constant_print["rad_filament"],2)
-    dist_trav=distance.cdist(info_array[-1][-1],info_coord[-1],'euclidean')
-    extrusion=round(float((area_road*dist_trav)/(fil_area)),5)
-    return extrusion
+    
+    #print("The info is",info_tuple[-1][-2])
+    dist_trav=distance.cdist(info_tuple[-1][-2],info_coord[-1],'euclidean')
+    extrusion=(float((area_road*dist_trav)/(fil_area)))
+    list_extrusion.append(extrusion)
+    sum_extrusion_array=np.sum(list_extrusion)
+    #print(list_extrusion)
+    return round(sum_extrusion_array,5)
 
-def coord_pump_on(param,pump_info): 
-    default_dict={"X":0,"Y":0,"Z":0}
+def coord_pump_off(param,pump_info):
+    global info_tuple,default_dict  
     num=1
     info_list=[] 
-    line_list=x.split()                          #convertes the splited lines into a dictionary with axis
+    line_list=param.split()                          #convertes the splited lines into a dictionary with axis
+    for i,lines in enumerate(line_list):
+        if (re.search(r"[A-Z]+\$[a-z]+\D[0-8]+\D",lines)):
+            del line_list[i]
+    for i,lines in enumerate(line_list):
+        if (re.search(r"[A-Z][0-1][0-1]",lines)):
+            del line_list[i]
+            break
+    #print (line_list)
+    
+    axis=["X","Y","Z"]
+    value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}        #creates a dictionary with X,Y and Z values
+    #print (value_dct)
+    
+    for keys in value_dct.keys():
+        default_dict[keys]=value_dct[keys]                                                              #keeps the default values in the dictionary
+    
+    info_axes=[tuple(float(default_dict[axes]) for axes in axis)]
+    
+    info_list.append(num)
+    info_list.append(pump_info)
+    info_list.append(info_axes)                                       #creates a list of tuple coordinates
+    info_list.append(0)
+    info_tuple.append(info_list)
+    return info_tuple
+
+def coord_pump_on(param,pump_info): 
+    global info_tuple,default_dict
+
+    num=1
+    info_list=[] 
+    line_list=param.split()                          #convertes the splited lines into a dictionary with axis
     for i,lines in enumerate(line_list):
         if (re.search(r"[A-Z]+\$[a-z]+\D[0-8]+\D",lines)):
             del line_list[i]
@@ -67,31 +106,63 @@ def coord_pump_on(param,pump_info):
     info_list.append(info_axes)
     #print(info_list)
     val_e=calculation(info_list)
-    temp_array=np.vstack((info_array,info_list))
+    info_list.append(val_e)
+    #print("The info List is:",info_list)
+    info_tuple.append(info_list)
+    #print("Are we here")
     #print(temp_array)
-    return temp_array
+    return info_tuple
     
 
 
-def take_conformal_code(line_value):                    #takes the location of the file
-    #with open(location,'r') as reader:
-        #for line in reader:
-            if (re.search(r"\$\D\w\[\d\]\.\X\=",line_value)):                   #check here
-                line_value=line_value.split()
-                pump_status=line_value[0][-1]
-            elif (int(pump_status)is 1):
-                coord_pump_on(line_value,pump_status)
-            elif (int(pump_status) is 0):
-                coord_pump_off(line_value,pump_staus)
+def output_line(file_location):
+    init_stat_1=["G92 E0","G92 E0","G1 Z0.300 F1200.000","G1 E-2.00000 F2400.00000","G92 E0"]    # This call should be written once at the top only
+    init_stat_2=["G1 E2.00000 F2400.00000","G1 F600"]                                             # Frequently called calls
+    global pump_status
+    with open (r"C:\Users\bb237\Gcode\example\output.txt",'a') as file:
+        file.seek(0) 
+        file.truncate()
 
+        with open (file_location,'r') as reader:
+            for stat in init_stat_1:
+                file.write(stat+"\n")
+            for line_value in reader:                    
+                    line_list=line_value.split()
+                    
+                    if (re.search(r"\$\D\w\[\d\]\.X\=",line_value)):                   #check here
+                        #print("i AM HERE AT PUMP STATUS")
+                        line_value=line_value.split()
+                        pump_status=line_value[0][-1]
+                        #print(pump_status)
+
+
+                    elif (line_list[0]=="G01"):
+                        #print("I am insided second choice")
+                        #print(pump_status)
+                        if (int(pump_status)== 1):
+                            #print("I am here at pump on")                                        #for pump on
+                            info_gcode=coord_pump_on(line_value,pump_status)
+                            axis=["X","Y","Z"]
+                            value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}  
+                            #print("I am at pump On:", line_list[0])
+                            output=line_list[0]+" "+" ".join(v+str(value_dct[v]) for v in value_dct)+ " E" + str(info_gcode[-1][-1])+"\n"
+                            file.write(output)
+                            
+                        elif (int(pump_status) == 0):
+                            #print("I am here at pump off")
+                            info_gcode=coord_pump_off(line_value,pump_status)
+                            axis=["X","Y","Z"]
+                            value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}  
+                            #print("I am at pump Off:", line_list[0])
+                            output=line_list[0]+" "+" ".join(v+str(value_dct[v]) for v in value_dct)+"\n"
+                            file.write(output)
+            
 
             
             
+output_line(r"C:\Users\bb237\Gcode\example\conformal_try_1.txt")
+for i in info_tuple:
+    print(i)
 
 
-
-
-info_array=convert_dictpara(x,1)
-
-#print(info_array)
 
--- try_2.py
+++ try_2.py
@@ -4,18 +4,27 @@ import math
 list_1=[(1,0,0)]
 list_2=[(0,1,0)]
 dm=distance.cdist(list_1,list_2,'euclidean')
-#$print(dm)
+info_extrusion=np.array([2])
 
+x="G01   X 19.808   Y 60.876   Z 3.478 F$global[8]"
+list_3=x.split()
+
+print(list_3)
+
+#$print(dm)
+'''dict_axes={"X":0,"Y":0,"Z":0}
 line="    $DO[8].X=1 "
 axis=["X","Y","Z"]
 
 list=["a","b",axis]
 
-#output="AB "+" ".join(v+str(dict_axes[v]) for v in dict_axes)
+output="AB "+" ".join(v+str(dict_axes[v]) for v in dict_axes)
 
-#print(output)
-line_value=line.split()
-print(int(line_value[0][-1]) is 1)
+print(output)
+
+#output=line_list[0]+" ".join(v+str(value_dct[v]) for v in value_dct.keys)+"\n"
+#line_value=line.split()
+#print(int(line_value[0][-1]) is 1)
 """
 constant_print={"layer_height":0.3, 
 "f_layer_height":0.4, 
@@ -28,4 +37,6 @@ constant_print={"layer_height":0.3,
 
 #area_road=(((constant_print["def_layer_width"]-constant_print["layer_height"])*constant_print["layer_height"])+(math.pi*math.pow(constant_print["layer_height"]/2,2)))
 fil_area=math.pi*math.pow(constant_print["rad_filament"],2)
-print (fil_area)"""
+
+print (fil_area)
+'''
