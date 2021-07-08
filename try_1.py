28a613740152de8a3a3cc892eda997cad862266a
kale.kaji <bsnbipendra@gmail.com>
2021-07-08 18:43:06 -0400

    Made all the necessary changes with the export command for pauses

--- try_1.py
+++ try_1.py
@@ -20,15 +20,19 @@ constant_print={"layer_height":0.3,
 "nozzle_dia":0.5, 
 }
 
+constant_speed={
+"pump_off":(15/4)*60,
+"pump_on":(20/4)*60,
+}
 
 dict_axes={"X":[],"Y":[],"Z":[]}
 
 
 list_extrusion=[]
 pump_status=0
-info_tuple=([1,0,[(0,0,0)],0])
+info_tuple=[[0,[(0,0,0)],0],[0,[(0,0,0)],0]]
 default_dict={"X":0,"Y":0,"Z":0}
-#print("At the start:",info_array)
+
 
 
 
@@ -37,12 +41,12 @@ def calculation(info_coord):
     area_road=(((constant_print["def_layer_width"]-constant_print["layer_height"])*constant_print["layer_height"])+(math.pi*math.pow(constant_print["layer_height"]/2,2)))
     fil_area=math.pi*math.pow(constant_print["rad_filament"],2)
     
-    #print("The info is",info_tuple[-1][-2])
+
     dist_trav=distance.cdist(info_tuple[-1][-2],info_coord[-1],'euclidean')
     extrusion=(float((area_road*dist_trav)/(fil_area)))
     list_extrusion.append(extrusion)
     sum_extrusion_array=np.sum(list_extrusion)
-    #print(list_extrusion)
+
     return round(sum_extrusion_array,5)
 
 def coord_pump_off(param,pump_info):
@@ -57,18 +61,18 @@ def coord_pump_off(param,pump_info):
         if (re.search(r"[A-Z][0-1][0-1]",lines)):
             del line_list[i]
             break
-    #print (line_list)
+
     
     axis=["X","Y","Z"]
     value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}        #creates a dictionary with X,Y and Z values
-    #print (value_dct)
+
     
     for keys in value_dct.keys():
         default_dict[keys]=value_dct[keys]                                                              #keeps the default values in the dictionary
     
     info_axes=[tuple(float(default_dict[axes]) for axes in axis)]
     
-    info_list.append(num)
+
     info_list.append(pump_info)
     info_list.append(info_axes)                                       #creates a list of tuple coordinates
     info_list.append(0)
@@ -88,11 +92,11 @@ def coord_pump_on(param,pump_info):
         if (re.search(r"[A-Z][0-1][0-1]",lines)):
             del line_list[i]
             break
-    #print (line_list)
+
     
     axis=["X","Y","Z"]
     value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}        #creates a dictionary with X,Y and Z values
-    #print (value_dct)
+
     
     for keys in value_dct.keys():
         default_dict[keys]=value_dct[keys]                                                              #keeps the default values in the dictionary
@@ -100,24 +104,23 @@ def coord_pump_on(param,pump_info):
     info_axes=[tuple(float(default_dict[axes]) for axes in axis)]                                       #creates a list of tuple coordinates
  
     
-    
-    info_list.append(num)
+
     info_list.append(pump_info)
     info_list.append(info_axes)
-    #print(info_list)
+
     val_e=calculation(info_list)
     info_list.append(val_e)
-    #print("The info List is:",info_list)
+
     info_tuple.append(info_list)
-    #print("Are we here")
-    #print(temp_array)
+
     return info_tuple
     
 
 
 def output_line(file_location):
     init_stat_1=["G92 E0","G92 E0","G1 Z0.300 F1200.000","G1 E-2.00000 F2400.00000","G92 E0"]    # This call should be written once at the top only
-    init_stat_2=["G1 E2.00000 F2400.00000","G1 F600"]                                             # Frequently called calls
+    init_stat_2=["G1 E2.00000 F2400.00000","G1 F600"]
+    init_speed=[" F"+ str(constant_speed["pump_off"])+"\n","G01"+" F"+ str(constant_speed["pump_on"])+"\n"]                                             # Frequently called calls
     global pump_status
     with open (r"C:\Users\bb237\Gcode\example\output.txt",'a') as file:
         file.seek(0) 
@@ -130,39 +133,46 @@ def output_line(file_location):
                     line_list=line_value.split()
                     
                     if (re.search(r"\$\D\w\[\d\]\.X\=",line_value)):                   #check here
-                        #print("i AM HERE AT PUMP STATUS")
+
+
                         line_value=line_value.split()
                         pump_status=line_value[0][-1]
-                        #print(pump_status)
+
 
 
                     elif (line_list[0]=="G01"):
-                        #print("I am insided second choice")
-                        #print(pump_status)
+
                         if (int(pump_status)== 1):
-                            #print("I am here at pump on")                                        #for pump on
                             info_gcode=coord_pump_on(line_value,pump_status)
                             axis=["X","Y","Z"]
                             value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}  
-                            #print("I am at pump On:", line_list[0])
-                            output=line_list[0]+" "+" ".join(v+str(value_dct[v]) for v in value_dct)+ " E" + str(info_gcode[-1][-1])+"\n"
-                            file.write(output)
+                         
+                            if(info_tuple[-2][0]==str(0)):
+ 
+                                output="G01"+" "+" ".join(v+str(value_dct[v]) for v in ["X","Y"])+ init_speed[0]
+                                file.write(output)
+                                output="G01"+" "+"Z"+value_dct["Z"]+ init_speed[0]
+                                file.write(output)
+                                file.write(init_speed[1])
+                            else:
+                                output="G01"+" "+" ".join(v+str(value_dct[v]) for v in value_dct)+ " E" + str(info_gcode[-1][-1])+"\n"
+                                file.write(output)
                             
                         elif (int(pump_status) == 0):
-                            #print("I am here at pump off")
+
                             info_gcode=coord_pump_off(line_value,pump_status)
                             axis=["X","Y","Z"]
                             value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}  
-                            #print("I am at pump Off:", line_list[0])
-                            output=line_list[0]+" "+" ".join(v+str(value_dct[v]) for v in value_dct)+"\n"
-                            file.write(output)
+   
+                            if(info_tuple[-2][0]== str(1)):
+                                output="G01"+" "+" ".join(v+str(value_dct[v]) for v in value_dct)+"\n"
+                                file.write(output)
             
 
             
             
 output_line(r"C:\Users\bb237\Gcode\example\conformal_try_1.txt")
-for i in info_tuple:
-    print(i)
+
 
 
 
--- try_2.py
+++ try_2.py
@@ -1,42 +1,6 @@
-from scipy.spatial import distance
-import numpy as np
-import math
-list_1=[(1,0,0)]
-list_2=[(0,1,0)]
-dm=distance.cdist(list_1,list_2,'euclidean')
-info_extrusion=np.array([2])
-
-x="G01   X 19.808   Y 60.876   Z 3.478 F$global[8]"
-list_3=x.split()
-
-print(list_3)
-
-#$print(dm)
-'''dict_axes={"X":0,"Y":0,"Z":0}
-line="    $DO[8].X=1 "
-axis=["X","Y","Z"]
-
-list=["a","b",axis]
-
-output="AB "+" ".join(v+str(dict_axes[v]) for v in dict_axes)
-
-print(output)
-
-#output=line_list[0]+" ".join(v+str(value_dct[v]) for v in value_dct.keys)+"\n"
-#line_value=line.split()
-#print(int(line_value[0][-1]) is 1)
-"""
-constant_print={"layer_height":0.3, 
-"f_layer_height":0.4, 
-"def_layer_width":0.5,
-"rad_filament":3.7, 
-"nozzle_dia":0.5, 
-}
-#print(math.pi*math.pow(constant_print["layer_height"]/2,2))
-#print(constant_print["def_layer_width"])
-
-#area_road=(((constant_print["def_layer_width"]-constant_print["layer_height"])*constant_print["layer_height"])+(math.pi*math.pow(constant_print["layer_height"]/2,2)))
-fil_area=math.pi*math.pow(constant_print["rad_filament"],2)
-
-print (fil_area)
-'''
+for i in "Bipendra":
+    print(i)
+    for j in "Basnet":
+        print(j)
+        break
+        break
