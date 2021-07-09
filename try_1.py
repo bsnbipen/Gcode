116276ace5f9472c252a4e4dac14d6276c389b41
bsnbipen <bsnbipendra@gmail.com>
2021-07-09 09:46:07 -0400

    Added the command for the first line

--- try_1.py
+++ try_1.py
@@ -30,7 +30,7 @@ dict_axes={"X":[],"Y":[],"Z":[]}
 
 list_extrusion=[]
 pump_status=0
-info_tuple=[[0,[(0,0,0)],0],[0,[(0,0,0)],0]]
+info_tuple=[[str(0),[(0,0,0)],0],[str(0),[(0,0,0)],0]]
 default_dict={"X":0,"Y":0,"Z":0}
 
 
@@ -117,16 +117,16 @@ def coord_pump_on(param,pump_info):
     
 
 
-def output_line(file_location):
+def output_line(input_file_location,output_file_location):
     init_stat_1=["G92 E0","G92 E0","G1 Z0.300 F1200.000","G1 E-2.00000 F2400.00000","G92 E0"]    # This call should be written once at the top only
     init_stat_2=["G1 E2.00000 F2400.00000","G1 F600"]
     init_speed=[" F"+ str(constant_speed["pump_off"])+"\n","G01"+" F"+ str(constant_speed["pump_on"])+"\n"]                                             # Frequently called calls
     global pump_status
-    with open (r"C:\Users\bb237\Gcode\example\output.txt",'a') as file:
+    with open (output_file_location,'a') as file:
         file.seek(0) 
         file.truncate()
 
-        with open (file_location,'r') as reader:
+        with open (input_file_location,'r') as reader:
             for stat in init_stat_1:
                 file.write(stat+"\n")
             for line_value in reader:                    
@@ -146,9 +146,10 @@ def output_line(file_location):
                             info_gcode=coord_pump_on(line_value,pump_status)
                             axis=["X","Y","Z"]
                             value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}  
-                         
+                            print("The val dct is hereP[1]:",value_dct)
+                            print("The value for info_tuple[-2][0]:",info_tuple[-2][0])
                             if(info_tuple[-2][0]==str(0)):
- 
+                                print("The val dct is here:",value_dct)
                                 output="G01"+" "+" ".join(v+str(value_dct[v]) for v in ["X","Y"])+ init_speed[0]
                                 file.write(output)
                                 output="G01"+" "+"Z"+value_dct["Z"]+ init_speed[0]
@@ -165,14 +166,15 @@ def output_line(file_location):
                             value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}  
    
                             if(info_tuple[-2][0]== str(1)):
+                                print("The value dct for off is here:",value_dct)
                                 output="G01"+" "+" ".join(v+str(value_dct[v]) for v in value_dct)+"\n"
                                 file.write(output)
             
 
             
             
-output_line(r"C:\Users\bb237\Gcode\example\conformal_try_1.txt")
-
+output_line(r"C:\Users\bipen\Documents\Python\project1\Gcode\Gcode\example\conformal_try_1.txt",r"C:\Users\bipen\Documents\Python\project1\Gcode\Gcode\example\output.txt")        #input file location
+        #change the output file location
 
 
 
