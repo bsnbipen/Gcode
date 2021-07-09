328f7e93054908a771e62291b29ba5412125d650
kale.kaji <bsnbipendra@gmail.com>
2021-07-09 10:11:35 -0400

    Added Initial extusion to 2

--- try_1.py
+++ try_1.py
@@ -28,9 +28,12 @@ constant_speed={
 dict_axes={"X":[],"Y":[],"Z":[]}
 
 
-list_extrusion=[]
+starting_point=(20,60.176,-3.631)                                           #the starting point for the print---    >Change For each Print<----
+
+
+list_extrusion=[2]
 pump_status=0
-info_tuple=[[str(0),[(0,0,0)],0],[str(0),[(0,0,0)],0]]
+info_tuple=[[str(0),[(0,0,0)],0],[str(0),[starting_point],0]]              #second element of this list is the starting point for the print
 default_dict={"X":0,"Y":0,"Z":0}
 
 
@@ -119,7 +122,7 @@ def coord_pump_on(param,pump_info):
 
 def output_line(input_file_location,output_file_location):
     init_stat_1=["G92 E0","G92 E0","G1 Z0.300 F1200.000","G1 E-2.00000 F2400.00000","G92 E0"]    # This call should be written once at the top only
-    init_stat_2=["G1 E2.00000 F2400.00000","G1 F600"]
+    init_stat_2=["G1 E2.00000 F2400.00000"+"\n","G1 F600"]
     init_speed=[" F"+ str(constant_speed["pump_off"])+"\n","G01"+" F"+ str(constant_speed["pump_on"])+"\n"]                                             # Frequently called calls
     global pump_status
     with open (output_file_location,'a') as file:
@@ -129,7 +132,7 @@ def output_line(input_file_location,output_file_location):
         with open (input_file_location,'r') as reader:
             for stat in init_stat_1:
                 file.write(stat+"\n")
-            for line_value in reader:                    
+            for i,line_value in enumerate(reader):                    
                     line_list=line_value.split()
                     
                     if (re.search(r"\$\D\w\[\d\]\.X\=",line_value)):                   #check here
@@ -143,13 +146,14 @@ def output_line(input_file_location,output_file_location):
                     elif (line_list[0]=="G01"):
 
                         if (int(pump_status)== 1):
+                            if(i==3):
+                                file.write(init_stat_2[0])
                             info_gcode=coord_pump_on(line_value,pump_status)
                             axis=["X","Y","Z"]
                             value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}  
-                            print("The val dct is hereP[1]:",value_dct)
-                            print("The value for info_tuple[-2][0]:",info_tuple[-2][0])
+                        
                             if(info_tuple[-2][0]==str(0)):
-                                print("The val dct is here:",value_dct)
+                         
                                 output="G01"+" "+" ".join(v+str(value_dct[v]) for v in ["X","Y"])+ init_speed[0]
                                 file.write(output)
                                 output="G01"+" "+"Z"+value_dct["Z"]+ init_speed[0]
@@ -166,14 +170,15 @@ def output_line(input_file_location,output_file_location):
                             value_dct={v:line_list[(i+1)] for i,v in enumerate(line_list) if v=="X" or v=="Y" or v=="Z"}  
    
                             if(info_tuple[-2][0]== str(1)):
-                                print("The value dct for off is here:",value_dct)
+                         
                                 output="G01"+" "+" ".join(v+str(value_dct[v]) for v in value_dct)+"\n"
                                 file.write(output)
             
 
-            
-            
-output_line(r"C:\Users\bipen\Documents\Python\project1\Gcode\Gcode\example\conformal_try_1.txt",r"C:\Users\bipen\Documents\Python\project1\Gcode\Gcode\example\output.txt")        #input file location
+
+
+
+output_line(r"C:\Users\bb237\Gcode\example\conformal_try_1.txt",r"C:\Users\bb237\Gcode\example\output.txt")        #input file location
         #change the output file location
 
 
